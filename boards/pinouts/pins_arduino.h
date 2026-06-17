d3423fabb04ebf12cf5a3eb39adb6f346cb26dc2
odoslf <odoslf@gmail.com>
2026-06-18 00:43:12 +0200

    Enable ODG S3 Bruce Full IR modules

--- boards/pinouts/pins_arduino.h
+++ boards/pinouts/pins_arduino.h
@@ -26,6 +26,8 @@
 #include "../m5stack-cplus1_1/pins_arduino.h"
 #elif ARDUINO_M5STICK_C_PLUS2
 #include "../m5stack-cplus2/pins_arduino.h"
+#elif ODG_S3_BRUCE_FULL
+#include "../ODG_S3_BRUCE_FULL/pins_arduino.h"
 #elif ESP32S3DEVKITC1
 #include "../ESP-General/pins_arduino.h"
 #elif SMOOCHIEE_BOARD
